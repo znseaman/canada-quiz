fc3ae82c46e1789ac19ad22ecc1eabddbf44951d
Zach Seaman <znseaman@gmail.com>
2020-03-05 15:08:33 -0800

    :sparkles: add timer to the top left corner

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import './App.css'
 import Progress from './components/Progress'
 import Question from './components/Question'
@@ -9,13 +9,52 @@ import NextButton from './components/NextButton'
 import StartButton from './components/StartButton'
 
 function App() {
-  const { state } = useStore()
-  const { currentQuestion, showResults, questions, isStarted } = state
+  const { state, setCurrentAnswer } = useStore()
+  const { currentQuestion, showResults, questions, isStarted, currentAnswer } = state
+  const startInterval = useRef(true)
+  const [secondsLeft, setSecondsLeft] = useState(10)
 
   const containerRef = useRef(null)
   useEffect(() => {
     containerRef.current.focus()
   }, [currentQuestion])
+
+  // countdown timer
+  useEffect(() => {
+    const timerInterval = setInterval(() => {
+      if (!isStarted) {
+        return false
+      }
+      if (!startInterval.current) {
+        return false
+      }
+      if (currentAnswer) {
+        return false
+      }
+      if (showResults) {
+        return false
+      }
+
+      // dispatch an action if time runs out
+      if (secondsLeft <= 0) {
+        setCurrentAnswer({ target: { value: "Unanswered" } })
+        return false
+      }
+
+      setSecondsLeft(secondsLeft - 1)
+    }, 1000)
+
+    return () => {
+      clearInterval(timerInterval)
+    }
+  }, [currentAnswer, isStarted, secondsLeft, setCurrentAnswer, showResults])
+
+  useEffect(() => {
+    if (currentAnswer) setSecondsLeft(10)
+    if (showResults) setSecondsLeft(10)
+  }, [currentAnswer, showResults])
+
+  const showSeconds = secondsLeft > 0 && !currentAnswer;
 
   return (
     <div className="container" tabIndex={"0"} ref={containerRef}>
@@ -30,6 +69,7 @@ function App() {
             <Results />
             :
             <>
+              {showSeconds && <div className="seconds">{secondsLeft}</div>}
               <Progress total={questions.length} current={currentQuestion + 1}></Progress>
               <Question question={questions[currentQuestion].question}></Question>
               <Answers />
